fd665282a1ac60df793fd71d0e97b10785bd5700
Kickcake <Kickvdstruik@gmail.com>
2023-04-12 11:07:47 +0200

    rip1

--- site/index.php
+++ site/index.php
@@ -16,6 +16,7 @@ if (mysqli_connect_error()) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
@@ -41,13 +42,6 @@ if (mysqli_connect_error()) {
             <?php } ?>
         </tbody>
     </table>
-    <div>
-        <?php foreach ($tools as $tool) { ?>
-            <div>
-                <p><?php echo $tool['tool_name'] ?></p>
-            </div>
-        <?php } ?>
-    </div>
 </body>
 
 </html>
