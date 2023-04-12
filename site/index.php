1c353847146877b3a572f48d4bfbf1551e7f11f0
Kickcake <Kickvdstruik@gmail.com>
2023-04-12 10:51:05 +0200

    d

--- site/index.php
+++ site/index.php
@@ -1,9 +1,31 @@
 <?php
 require 'database.php';
-$sql = "SELECT tool_name FROM `tools` WHERE tool_id = 1;";
+$sql = "SELECT * FROM `tools`;";
 $result = mysqli_query($conn, $sql);
 if (mysqli_connect_error()) {
     echo "orh no";
 } else {
     echo "fotknighte gg";
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <?php foreach ($tools as $tool) : ?>
+            <div>
+                <p><?php echo $result['name'] ?></p>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</body>
+
+</html>
